522f6778a5fe1bc00b43ec97afd2d04cae37e60a
SerhiiYaroshenko1986 <datextdk@gmail.com>
2020-01-30 15:49:23 +0200

    first commit

---
+++ web/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#all").on("click", function() {
+        $("#open").toggle();
+    })
+    $("#popUp").on("click", function() {
+        $("#show").toggle("slow");
+    })
+
+})
